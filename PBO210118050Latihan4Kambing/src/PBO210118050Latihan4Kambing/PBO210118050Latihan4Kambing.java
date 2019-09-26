1f56cc68905342d54148194ce0b9029572ea1361
M-Rizky-permana <55745444+M-Rizky-permana@users.noreply.github.com>
2019-09-26 19:57:40 +0700

    Add files via upload

---
+++ PBO210118050Latihan4Kambing/src/PBO210118050Latihan4Kambing/PBO210118050Latihan4Kambing.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PBO210118050Latihan4Kambing;
+
+/**
+ *
+ * @author 
+ * NAMA     : Muhamad Rizky Permana
+ * KELAS    : IF-2
+ * NIM      : 10118050
+ * Deskripsi Program : Menampilkan data kambing
+ */
+public class PBO210118050Latihan4Kambing {
+
+    public void tambahKambing(){
+    int JumlahKambing = 0;
+        
+    JumlahKambing = JumlahKambing + 5;
+        
+    System.out.println("Jumlah kambing setelah di tambah : " + JumlahKambing);
+    
+    }
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+        PBO210118050Latihan4Kambing kambingJantan = new PBO210118050Latihan4Kambing();
+        kambingJantan.tambahKambing();
+        
+        
+        
+    }
+}
